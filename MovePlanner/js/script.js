bd5007cef4c33c0946ec8e7da69e87b3a7b14a6c
subdomestic <subdomestic@gmail.com>
2015-10-26 11:37:18 -0500

    adjust wiki API link

--- MovePlanner/js/script.js
+++ MovePlanner/js/script.js
@@ -50,7 +50,7 @@ function loadData() {
     // load wikipedia data
 
     $.ajax({
-    url: "https://en.wikipedia.org/w/api.php?action=query&titles=" + cityStr + "&format=json",
+    url: "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=jsonfm&titles=" + cityStr,
  
     // Tell jQuery we're expecting JSONP
     dataType: "jsonp",
