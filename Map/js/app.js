0a1ccdeeb823b7ad85b8935a46526186e2b46810
subdomestic <subdomestic@gmail.com>
2015-11-27 16:49:00 -0500

    working on markers

--- Map/js/app.js
+++ Map/js/app.js
@@ -2,31 +2,36 @@ var locations = [{
         name: 'Plaza de Bolívar',
         latitude: 4.5981259,
         longitude: -74.0782322,
-        description: 'Presidencia'
+        description: 'Presidencia',
+        marker:  1
     },
     {
         name: 'Museo del 20 de Julio',
         latitude: 4.5982801,
         longitude: -74.0772751,
-        description: 'https://www.flickr.com/photos/bigtallguy'
+        description: 'https://www.flickr.com/photos/bigtallguy',
+        marker: 1
     },
     {
         name: 'Museo Nacional',
         latitude: 4.6154977,
         longitude: -74.0704757,
-        description: 'https://www.flickr.com/photos/bigtallguy'
+        description: 'https://www.flickr.com/photos/bigtallguy',
+        marker: 1
     },
     {
         name: 'Planetario Distrital',
         latitude: 4.6120873,
         longitude: -74.0709737,
-        description: 'https://www.flickr.com/photos/bigtallguy'
+        description: 'https://www.flickr.com/photos/bigtallguy',
+        marker: 1
     },
     {
         name: 'Monserrate',
         latitude: 4.6012648,
         longitude: -74.0671971,
-        description: 'https://www.flickr.com/photos/bigtallguy'
+        description: 'https://www.flickr.com/photos/bigtallguy',
+        marker: 1
     }
 ];
 
@@ -36,31 +41,49 @@ var place = function(data){
     this.lat= ko.observable(data.latitude);
     this.long = ko.observable(data.longitude);
     this.description = ko.observable(data.description);
+    this.marker = ko.observable(data.marker);
 
-    this.marker = new google.maps.Marker({
-    position: new google.maps.LatLng(this.lat, this.long),
-    animation: google.maps.Animation.DROP
-  });
 }
 
-//global for map
-var map;
+//global for map and markers
+var map, markers;
 
-$(document).ready(function () {
-   
-   //loadMap();
-   //ko.applyBindings(viewModel);
-});
-
-function MyViewModel() {
+var MyViewModel = function() {
     var self = this;
-    
-    this.locationsList = ko.observableArray([]);
+    var locationsList = ko.observableArray([]);
+    filter = ko.observable("");
 
     locations.forEach(function(Item){
-        self.locationsList.push(new place(Item));
+        locationsList.push(new place(Item));
     });
-}
+
+    console.log("mark" + locationsList());
+    ko.utils.arrayForEach(locationsList(), function(){
+        this.marker = displayMarker(this.latitude, this.longitude); 
+        console.log("m" + this.marker);
+    })
+    filteredItems = ko.dependentObservable(function() {
+
+        var filter = parent.filter().toLowerCase();
+        var filteredList = ko.observableArray([]);
+        console.log("a" + filter);
+        if (!filter) {
+            return locationsList();
+        } else {
+            return ko.utils.arrayFilter(locationsList(), function(item) {
+                return ko.utils.stringStartsWith(item.name().toLowerCase(), filter)
+            });
+        }
+}, self);
+    
+    filteredItems.subscribe(function(){
+        console.log("Filtered Items!");
+        console.log(filteredItems().length);
+        console.log
+    })
+    
+};
+
 
 function loadMap() {
         map = new google.maps.Map(document.getElementById("map-div"), {
@@ -68,11 +91,11 @@ function loadMap() {
          zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-/*
+
     locations.forEach(function(Item){
-        displayMarker(Item.latitude, Item.longitude);
+        Item.marker = displayMarker(Item.latitude, Item.longitude);   
     });
-*/
+
 }
 
 function displayMarker(lat, long){
@@ -82,27 +105,28 @@ function displayMarker(lat, long){
         map: map,
         position: position
     });
+
+    return marker
+/*
+    this.isVisible = ko.observable(false);
+
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+    }
+  });
+
+  this.isVisible(true);*/
 }
 
-/*
-ko.bindingHandlers.map = {
-            init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+//I had the error message: utils.stringStartsWith is not a function and found the solution here https://groups.google.com/forum/#!topic/knockoutjs/y-oow7ZzLjo
+  ko.utils.stringStartsWith = function (string, startsWith) {           
+            string = string || "";
+            if (startsWith.length > string.length)
+                return false;
+            return string.substring(0, startsWith.length) === startsWith;
+        }
 
-
-                //var position = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
-
-                    locations.forEach(function(Item){
-                         displayMarker(Item.latitude, Item.longitude);
-                     });
-
-                viewModel._mapMarker = marker;
-            },
-            update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-                var latlng = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
-                viewModel._mapMarker.setPosition(latlng);
-
-            }
-        };
-
-*/
-ko.applyBindings(new MyViewModel());
+ko.applyBindings(new MyViewModel);
